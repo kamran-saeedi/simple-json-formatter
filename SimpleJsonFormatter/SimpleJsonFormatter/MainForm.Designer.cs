90b625f0a3f6efe379ef14d9cd5183bafd6db3d1
kamran-saeedi <kamran_saeedi@yahoo.com>
2018-05-29 13:25:20 +0430

    Add files via upload

---
+++ SimpleJsonFormatter/SimpleJsonFormatter/MainForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace SimpleJsonFormatter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.scnOutput = new ScintillaNET.Scintilla();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnPaste = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnCollapseAll = new System.Windows.Forms.ToolStripButton();
+            this.btnExapndAll = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnShowWhitespace = new System.Windows.Forms.ToolStripButton();
+            this.btnAutoPasteOnLoad = new System.Windows.Forms.ToolStripButton();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtInput.Location = new System.Drawing.Point(0, 0);
+            this.txtInput.MaxLength = 1000000;
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInput.Size = new System.Drawing.Size(529, 727);
+            this.txtInput.TabIndex = 2;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // scnOutput
+            // 
+            this.scnOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scnOutput.Location = new System.Drawing.Point(0, 0);
+            this.scnOutput.Name = "scnOutput";
+            this.scnOutput.Size = new System.Drawing.Size(1054, 727);
+            this.scnOutput.TabIndex = 4;
+            this.scnOutput.UpdateUI += new System.EventHandler<ScintillaNET.UpdateUIEventArgs>(this.scnOutput_UpdateUI);
+            this.scnOutput.TextChanged += new System.EventHandler(this.scnOutput_TextChanged);
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.splitContainer1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(1587, 727);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(1587, 752);
+            this.toolStripContainer1.TabIndex = 5;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitter1);
+            this.splitContainer1.Panel1.Controls.Add(this.txtInput);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.scnOutput);
+            this.splitContainer1.Size = new System.Drawing.Size(1587, 727);
+            this.splitContainer1.SplitterDistance = 529;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 727);
+            this.splitter1.TabIndex = 3;
+            this.splitter1.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnPaste,
+            this.toolStripSeparator2,
+            this.btnCollapseAll,
+            this.btnExapndAll,
+            this.toolStripSeparator1,
+            this.btnShowWhitespace,
+            this.btnAutoPasteOnLoad});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(427, 25);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnPaste.Image = ((System.Drawing.Image)(resources.GetObject("btnPaste.Image")));
+            this.btnPaste.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(23, 22);
+            this.btnPaste.Text = "&Paste";
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnCollapseAll
+            // 
+            this.btnCollapseAll.AutoToolTip = false;
+            this.btnCollapseAll.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnCollapseAll.Image = ((System.Drawing.Image)(resources.GetObject("btnCollapseAll.Image")));
+            this.btnCollapseAll.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCollapseAll.Name = "btnCollapseAll";
+            this.btnCollapseAll.Size = new System.Drawing.Size(73, 22);
+            this.btnCollapseAll.Text = "Collapse All";
+            this.btnCollapseAll.Click += new System.EventHandler(this.btnCollapseAll_Click);
+            // 
+            // btnExapndAll
+            // 
+            this.btnExapndAll.AutoToolTip = false;
+            this.btnExapndAll.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnExapndAll.Image = ((System.Drawing.Image)(resources.GetObject("btnExapndAll.Image")));
+            this.btnExapndAll.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExapndAll.Name = "btnExapndAll";
+            this.btnExapndAll.Size = new System.Drawing.Size(66, 22);
+            this.btnExapndAll.Text = "Expand All";
+            this.btnExapndAll.Click += new System.EventHandler(this.btnExapndAll_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnShowWhitespace
+            // 
+            this.btnShowWhitespace.CheckOnClick = true;
+            this.btnShowWhitespace.Image = global::SimpleJsonFormatter.Properties.Resources.chkbx0;
+            this.btnShowWhitespace.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnShowWhitespace.Name = "btnShowWhitespace";
+            this.btnShowWhitespace.Size = new System.Drawing.Size(120, 22);
+            this.btnShowWhitespace.Text = "Show Whitespace";
+            this.btnShowWhitespace.CheckedChanged += new System.EventHandler(this.btnShowWhitespace_CheckedChanged);
+            // 
+            // btnAutoPasteOnLoad
+            // 
+            this.btnAutoPasteOnLoad.CheckOnClick = true;
+            this.btnAutoPasteOnLoad.Image = global::SimpleJsonFormatter.Properties.Resources.chkbx1;
+            this.btnAutoPasteOnLoad.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAutoPasteOnLoad.Name = "btnAutoPasteOnLoad";
+            this.btnAutoPasteOnLoad.Size = new System.Drawing.Size(130, 22);
+            this.btnAutoPasteOnLoad.Text = "Auto Paste on Load";
+            this.btnAutoPasteOnLoad.CheckedChanged += new System.EventHandler(this.btnAutoPasteOnLoad_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1587, 752);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Simple Json Formatter";
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox txtInput;
+        private ScintillaNET.Scintilla scnOutput;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnCollapseAll;
+        private System.Windows.Forms.ToolStripButton btnExapndAll;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnShowWhitespace;
+        private System.Windows.Forms.ToolStripButton btnPaste;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnAutoPasteOnLoad;
+    }
+}
+
